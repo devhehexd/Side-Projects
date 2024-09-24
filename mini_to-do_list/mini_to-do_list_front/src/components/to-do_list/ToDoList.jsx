e5c659d8875451ed215f19c0f3666c56cbdfa3a9
cruz <rlqja72201@gmail.com>
2024-09-24 12:00:26 +0900

    build

--- mini_to-do_list/mini_to-do_list_front/src/components/to-do_list/ToDoList.jsx
+++ mini_to-do_list/mini_to-do_list_front/src/components/to-do_list/ToDoList.jsx
@@ -12,6 +12,14 @@ function ToDoList() {
 
   const [userId, setUserId] = useState(null);
 
+  const dateFormat = (postDate) => {
+    const date = new Date(postDate);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0')
+    return `${year}-${month}-${day}`;
+  }
+
   useEffect(() => {
     if (token) {
 
@@ -57,6 +65,7 @@ function ToDoList() {
           <tr>
             <th>번호</th>
             <th>To-Do</th>
+            <th>D-Day</th>
           </tr>
         </thead>
         <tbody>
@@ -66,6 +75,7 @@ function ToDoList() {
               <tr key={idx}>
                 <td>{item.number}</td>
                 <td><Link to={'/details/' + item.number}>{item.toDo}</Link></td>
+                <td>{item.dDay} - {dateFormat(item.postDate)}</td>
                 <td><button type="button" onClick={() => deleteToDo(item.number)} >삭제</button></td>
               </tr>
             ))}
